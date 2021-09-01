235aafb4cf8f0ad55a7be0b2e37585c38aaa220e
VikasSinghNegi <vikasnegi808@gmail.com>
2021-09-01 13:54:22 +0530

    1 file modified

--- src/app/account/register.component.spec.ts
+++ src/app/account/register.component.spec.ts
@@ -1,8 +1,3 @@
-// describe('Testing tests', () => {
-//   it('should succeed', () => expect(true).toEqual(true));
-//   it('should fail', () => expect(true).toEqual(false));
-// });
-
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -58,5 +53,89 @@ describe('StaticComponent', () => {
     });
     expect(component.form.invalid).toBeTruthy();
     expect(component.form.controls['firstName'].errors.minlength).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'testing',
+      lastName: 'lastname',
+      phone: '9542365897',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeFalsy();
+  });
+
+  it('should check Last Name Validation', () => {
+    component.form.patchValue({
+      firstName: 'firstName',
+      lastName: '',
+      phone: '9542365897',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['lastName'].errors.required).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'firstName',
+      lastName: 'last',
+      phone: '9542365897',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['lastName'].errors.minlength).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'firstName',
+      lastName: 'lastname',
+      phone: '9542365897',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeFalsy();
+  });
+
+  it('should check Phone Validation', () => {
+    component.form.patchValue({
+      firstName: 'firstname',
+      lastName: 'lastname',
+      phone: '',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['phone'].errors.required).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'firstname',
+      lastName: 'lastname',
+      phone: '95423',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['phone'].errors.minlength).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'firstname',
+      lastName: 'lastname',
+      phone: '95423658568',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['phone'].errors.maxlength).toBeTruthy();
+  });
+
+  it('should check Country Validation', () => {
+    component.form.patchValue({
+      firstName: 'firstName',
+      lastName: 'lastname',
+      phone: '9542365897',
+      country: ''
+    });
+    expect(component.form.invalid).toBeTruthy();
+    expect(component.form.controls['country'].errors.required).toBeTruthy();
+
+    component.form.patchValue({
+      firstName: 'firstName',
+      lastName: 'lastname',
+      phone: '9542365897',
+      country: 'India'
+    });
+    expect(component.form.invalid).toBeFalsy();
   });
 });
